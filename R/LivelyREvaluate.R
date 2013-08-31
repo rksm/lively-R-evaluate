0ddb623459860c4559e3c085e74975c4f993dc0f
Robert Krahn <robert.krahn@gmail.com>
2013-08-30 17:45:46 -0700

    making parallel evaluation work

--- R/LivelyREvaluate.R
+++ R/LivelyREvaluate.R
@@ -1,5 +1,7 @@
 require(evaluate)
 require(rjson)
+require(parallel)
+require(tools)
 
 # helper for interactive package development
 reload <- function( path ) {
@@ -58,32 +60,53 @@ getEvalResults = function() {
 collectEvalProc = function(id) {
     proc = evalProcs[[id]]
     if (is.null(proc)) return(NULL)
-    result = multicore::collect(proc, wait=FALSE)
+    result = parallel::mccollect(proc, wait=FALSE)
+    message(paste("Collecting:", toString(result)))
     if (!is.null(result)) remove(list=c(id), envir=evalProcs)
-    result[[1]]
+    if (inherits(result,"try-error")) result
+    else result[[1]]
+}
+
+killEvalProc = function(id) {
+    proc = evalProcs[[id]]
+    if (!is.null(proc)) tools::pskill(proc$pid)
 }
 
 # return a specific eval result
-getEvalResult = function(id, format="R") {
+getEvalResult = function(id, format="R", file=NULL) {
     result = evalResults[[id]]
     if (is.null(result)) {
         result = collectEvalProc(id)
         if (!is.null(result)) evalResults[[id]] = result
     }
+    if (inherits(result,"try-error")) result = toString(result)
+    else result = result$result
     if (format == "JSON") result = rjson::toJSON(result)
+    if (!is.null(file)) {
+        write(result, file = file)
+    }
     result
 }
 
 isDone = function(id) {
     result = getEvalResult(id)
-    !is.null(result) && endMarker %in% result$result$source
+    !is.null(result) && (inherits(result, "try-error") || endMarker %in% result$result$source)
+}
+
+.whenEvalFinished = function(evalResult) {
+    parallel:::sendMaster(evalResult)
+    quit(save = "no", status = 0, runLast = FALSE)
 }
 
 # here we start the evaluation and define the output_handler used by
 # evaluate::evaluate. Returns an evalState 
-.evaluateString <- function(evalId, string) {
-    # we use the data.frame for capturing eval outputs
+.evaluateString <- function(evalId, string, exit=FALSE) {
     evalState = createEvalState(id=evalId, source=string)
+    if (exit) {
+        # when we are evaluating in a separate process make sure that we quit
+        # the process when eval is done
+        on.exit(do.call(".whenEvalFinished", list(evalState)))
+    }
     recordResult = function(type, value) {
         if (type == "source") {
             evalState$statementIndex <<- evalState$statementIndex + 1
@@ -98,7 +121,10 @@ isDone = function(id) {
         warning = function(x){ recordResult('warning', x); },
         error = function(x){ recordResult('error', x); },
         value = function(val){ recordResult('value', val); return(val) })
-    evaluate:::evaluate(paste(string, endMarker, sep="\n"), stop_on_error=1, output_handler=evalHandler)
+    evaluate:::evaluate(
+        paste(string, endMarker, sep="\n"),
+        stop_on_error=1,
+        output_handler=evalHandler)
     return(evalState)
 }
 
@@ -110,11 +136,17 @@ evaluateString <- function(evalId, string) {
 
 # asynchronous eval
 evaluateStringParallel <- function(id, string) {
-    evalProcs[[id]] <<- multicore::parallel(.evaluateString(id, string), name=id)
+    evalProcs[[id]] = parallel::mcparallel(
+        .evaluateString(id, string, exit=TRUE),
+        name=id)
 }
 
-evaluateJSON <- function(json) {
+evaluateJSON <- function(json,fork=TRUE) {
     # json = {id: STRING, source: STRING}
     jso = as.environment(rjson::fromJSON(json)) # make it a hash
-    evaluateString(jso$id, jso$source)
+    if (fork) {
+        evaluateStringParallel(jso$id, jso$source)
+    } else {
+        evaluateString(jso$id, jso$source)
+    }
 }
